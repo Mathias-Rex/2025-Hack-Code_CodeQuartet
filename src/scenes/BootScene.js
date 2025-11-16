2260588b28ee919c8dc65fea457b87fdaefbee06
geygibue <bozzay.peter2012@gmail.com>
2025-11-16 17:30:13 +0100

    effekt ki/be

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -2,7 +2,7 @@ export default class BootScene extends Phaser.Scene {
   constructor() { super('Boot'); }
   create() {
     if (!window.__GAME_SETTINGS__) {
-      window.__GAME_SETTINGS__ = { musicEnabled: true };
+      window.__GAME_SETTINGS__ = { musicEnabled: true, sfxEnabled: true };
     }
     this.cameras.main.setBackgroundColor('#1d212d');
     this.scene.start('Preload');
